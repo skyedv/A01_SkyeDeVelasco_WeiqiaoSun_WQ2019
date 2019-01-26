3057c61553bbf6b71db7df5bad5a4551ba6aa537
jerrysunwx <46798404+jerrysunwx@users.noreply.github.com>
2019-01-25 17:16:24 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -35,158 +35,319 @@
 
 //*****************************************************************************
 //
-// Application Name     - Blinky
-// Application Overview - The objective of this application is to showcase the 
-//                        GPIO control using Driverlib api calls. The LEDs 
-//                        connected to the GPIOs on the LP are used to indicate 
-//                        the GPIO output. The GPIOs are driven high-low 
-//                        periodically in order to turn on-off the LEDs.
+// Application Name     - SPI Demo
+// Application Overview - The demo application focuses on showing the required 
+//                        initialization sequence to enable the CC3200 SPI 
+//                        module in full duplex 4-wire master and slave mode(s).
 // Application Details  -
-// http://processors.wiki.ti.com/index.php/CC32xx_Blinky_Application
+// http://processors.wiki.ti.com/index.php/CC32xx_SPI_Demo
 // or
-// docs\examples\CC32xx_Blinky_Application.pdf
+// docs\examples\CC32xx_SPI_Demo.pdf
 //
 //*****************************************************************************
 
-//****************************************************************************
+
+//*****************************************************************************
 //
-//! \addtogroup blinky
+//! \addtogroup SPI_Demo
 //! @{
 //
-//****************************************************************************
+//*****************************************************************************
 
 // Standard includes
-#include <stdio.h>
+#include <string.h>
 
 // Driverlib includes
 #include "hw_types.h"
-#include "hw_ints.h"
 #include "hw_memmap.h"
 #include "hw_common_reg.h"
-#include "interrupt.h"
-#include "hw_apps_rcm.h"
-#include "prcm.h"
+#include "hw_ints.h"
+#include "spi.h"
 #include "rom.h"
 #include "rom_map.h"
-#include "prcm.h"
-#include "gpio.h"
 #include "utils.h"
+#include "prcm.h"
 #include "uart.h"
+#include "interrupt.h"
 
 // Common interface includes
-#include "gpio_if.h"
-
+#include "uart_if.h"
 #include "pin_mux_config.h"
 
+
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1351.h"
+#include "glcdfont.h"
+
 #define APPLICATION_VERSION     "1.1.1"
+//*****************************************************************************
+//
+// Application Master/Slave mode selector macro
+//
+// MASTER_MODE = 1 : Application in master mode
+// MASTER_MODE = 0 : Application in slave mode
+//
+//*****************************************************************************
+#define MASTER_MODE      0
+
+#define SPI_IF_BIT_RATE  100000
+#define TR_BUFF_SIZE     100
+
+#define MASTER_MSG       "This is CC3200 SPI Master Application\n\r"
+#define SLAVE_MSG        "This is CC3200 SPI Slave Application\n\r"
 
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- Start
 //*****************************************************************************
+static unsigned char g_ucTxBuff[TR_BUFF_SIZE];
+static unsigned char g_ucRxBuff[TR_BUFF_SIZE];
+static unsigned char ucTxBuffNdx;
+static unsigned char ucRxBuffNdx;
+
 #if defined(ccs)
 extern void (* const g_pfnVectors[])(void);
 #endif
 #if defined(ewarm)
 extern uVectorEntry __vector_table;
 #endif
+
+
+// Color definitions
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define RED             0xF800
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0
+#define WHITE           0xFFFF
+//
+
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- End
 //*****************************************************************************
 
 
-//*****************************************************************************
-//                      LOCAL FUNCTION PROTOTYPES                           
-//*****************************************************************************
-void LEDBlinkyRoutine();
-static void BoardInit(void);
-
-//*****************************************************************************
-//                      LOCAL FUNCTION DEFINITIONS                         
-//*****************************************************************************
 
 //*****************************************************************************
 //
-//! Configures the pins as GPIOs and peroidically toggles the lines
+//! SPI Slave Interrupt handler
 //!
-//! \param None
-//! 
-//! This function  
-//!    1. Configures 3 lines connected to LEDs as GPIO
-//!    2. Sets up the GPIO pins as output
-//!    3. Periodically toggles each LED one by one by toggling the GPIO line
+//! This function is invoked when SPI slave has its receive register full or
+//! transmit register empty.
 //!
-//! \return None
+//! \return None.
 //
 //*****************************************************************************
-void LEDFlashRoutine()
+static void SlaveIntHandler()
 {
-    //
-    // Toggle the lines initially to turn off the LEDs.
-    // The values driven are as required by the LEDs on the LP.
-    //
+    unsigned long ulRecvData;
+    unsigned long ulStatus;
 
-    GPIO_IF_LedOff(MCU_ALL_LED_IND);
-    MAP_UtilsDelay(8000000);
-    GPIO_IF_LedOn(MCU_RED_LED_GPIO);
-    GPIO_IF_LedOn(MCU_ORANGE_LED_GPIO);
-    GPIO_IF_LedOn(MCU_GREEN_LED_GPIO);
-    MAP_UtilsDelay(8000000);
-    GPIO_IF_LedOff(MCU_RED_LED_GPIO);
-    GPIO_IF_LedOff(MCU_ORANGE_LED_GPIO);
-    GPIO_IF_LedOff(MCU_GREEN_LED_GPIO);
-    MAP_UtilsDelay(8000000);
+    ulStatus = MAP_SPIIntStatus(GSPI_BASE,true);
 
-}
+    MAP_SPIIntClear(GSPI_BASE,SPI_INT_RX_FULL|SPI_INT_TX_EMPTY);
 
-void LEDBlinkyRoutine()
-{
-    //
-    // Toggle the lines initially to turn off the LEDs.
-    // The values driven are as required by the LEDs on the LP.
-    //
-
-    GPIO_IF_LedOff(MCU_ALL_LED_IND);
-
-
-
-        //
-        // Alternately toggle hi-low each of the GPIOs
-        // to switch the corresponding LED on/off.
-        //
-
-        //zero
-        MAP_UtilsDelay(8000000);
-        //one
-        GPIO_IF_LedOn(MCU_RED_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        //two
-        GPIO_IF_LedOff(MCU_RED_LED_GPIO);
-        GPIO_IF_LedOn(MCU_ORANGE_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        //three
-        GPIO_IF_LedOn(MCU_RED_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        //four
-        GPIO_IF_LedOff(MCU_RED_LED_GPIO);
-        GPIO_IF_LedOff(MCU_ORANGE_LED_GPIO);
-        GPIO_IF_LedOn(MCU_GREEN_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        //five
-        GPIO_IF_LedOn(MCU_RED_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        //six
-        GPIO_IF_LedOff(MCU_RED_LED_GPIO);
-        GPIO_IF_LedOn(MCU_ORANGE_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        //seven
-        GPIO_IF_LedOn(MCU_RED_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        GPIO_IF_LedOff(MCU_RED_LED_GPIO);
-        GPIO_IF_LedOff(MCU_ORANGE_LED_GPIO);
-        GPIO_IF_LedOff(MCU_GREEN_LED_GPIO);
-
+    if(ulStatus & SPI_INT_TX_EMPTY)
+    {
+        MAP_SPIDataPut(GSPI_BASE,g_ucTxBuff[ucTxBuffNdx%TR_BUFF_SIZE]);
+        ucTxBuffNdx++;
     }
 
+    if(ulStatus & SPI_INT_RX_FULL)
+    {
+        MAP_SPIDataGetNonBlocking(GSPI_BASE,&ulRecvData);
+        g_ucTxBuff[ucRxBuffNdx%TR_BUFF_SIZE] = ulRecvData;
+        Report("%c",ulRecvData);
+        ucRxBuffNdx++;
+    }
+}
+
+//*****************************************************************************
+//
+//! SPI Master mode main loop
+//!
+//! This function configures SPI modelue as master and enables the channel for
+//! communication
+//!
+//! \return None.
+//
+//*****************************************************************************
+void MasterMain()
+{
+
+    unsigned long ulUserData;
+    unsigned long ulDummy;
+
+    //
+    // Initialize the message
+    //
+    memcpy(g_ucTxBuff,MASTER_MSG,sizeof(MASTER_MSG));
+
+    //
+    // Set Tx buffer index
+    //
+    ucTxBuffNdx = 0;
+    ucRxBuffNdx = 0;
+
+    //
+    // Reset SPI
+    //
+    MAP_SPIReset(GSPI_BASE);
+
+    //
+    // Configure SPI interface
+    //
+    MAP_SPIConfigSetExpClk(GSPI_BASE,MAP_PRCMPeripheralClockGet(PRCM_GSPI),
+                     SPI_IF_BIT_RATE,SPI_MODE_MASTER,SPI_SUB_MODE_0,
+                     (SPI_SW_CTRL_CS |
+                     SPI_4PIN_MODE |
+                     SPI_TURBO_OFF |
+                     SPI_CS_ACTIVEHIGH |
+                     SPI_WL_8));
+
+    //
+    // Enable SPI for communication
+    //
+    MAP_SPIEnable(GSPI_BASE);
+
+    //
+    // Print mode on uart
+    //
+    Message("Enabled SPI Interface in Master Mode\n\r");
+
+    //
+    // User input
+    //
+    Report("Press any key to transmit data....");
+
+    //
+    // Read a character from UART terminal
+    //
+    ulUserData = MAP_UARTCharGet(UARTA0_BASE);
+
+
+    //
+    // Send the string to slave. Chip Select(CS) needs to be
+    // asserted at start of transfer and deasserted at the end.
+    //
+    MAP_SPITransfer(GSPI_BASE,g_ucTxBuff,g_ucRxBuff,50,
+            SPI_CS_ENABLE|SPI_CS_DISABLE);
+
+    //
+    // Report to the user
+    //
+    Report("\n\rSend      %s",g_ucTxBuff);
+    Report("Received  %s",g_ucRxBuff);
+
+    //
+    // Print a message
+    //
+    Report("\n\rType here (Press enter to exit) :");
+
+    //
+    // Initialize variable
+    //
+    ulUserData = 0;
+
+    //
+    // Enable Chip select
+    //
+    MAP_SPICSEnable(GSPI_BASE);
+
+    //
+    // Loop until user "Enter Key" is
+    // pressed
+    //
+    while(ulUserData != '\r')
+    {
+        //
+        // Read a character from UART terminal
+        //
+        ulUserData = MAP_UARTCharGet(UARTA0_BASE);
+
+        //
+        // Echo it back
+        //
+        MAP_UARTCharPut(UARTA0_BASE,ulUserData);
+
+        //
+        // Push the character over SPI
+        //
+        MAP_SPIDataPut(GSPI_BASE,ulUserData);
+
+        //
+        // Clean up the receive register into a dummy
+        // variable
+        //
+        MAP_SPIDataGet(GSPI_BASE,&ulDummy);
+    }
+
+    //
+    // Disable chip select
+    //
+    MAP_SPICSDisable(GSPI_BASE);
+}
+
+//*****************************************************************************
+//
+//! SPI Slave mode main loop
+//!
+//! This function configures SPI modelue as slave and enables the channel for
+//! communication
+//!
+//! \return None.
+//
+//*****************************************************************************
+void SlaveMain()
+{
+    //
+    // Initialize the message
+    //
+    memcpy(g_ucTxBuff,SLAVE_MSG,sizeof(SLAVE_MSG));
+
+    //
+    // Set Tx buffer index
+    //
+    ucTxBuffNdx = 0;
+    ucRxBuffNdx = 0;
+
+    //
+    // Reset SPI
+    //
+    MAP_SPIReset(GSPI_BASE);
+
+    //
+    // Configure SPI interface
+    //
+    MAP_SPIConfigSetExpClk(GSPI_BASE,MAP_PRCMPeripheralClockGet(PRCM_GSPI),
+                     SPI_IF_BIT_RATE,SPI_MODE_SLAVE,SPI_SUB_MODE_0,
+                     (SPI_HW_CTRL_CS |
+                     SPI_4PIN_MODE |
+                     SPI_TURBO_OFF |
+                     SPI_CS_ACTIVEHIGH |
+                     SPI_WL_8));
+
+    //
+    // Register Interrupt Handler
+    //
+    MAP_SPIIntRegister(GSPI_BASE,SlaveIntHandler);
+
+    //
+    // Enable Interrupts
+    //
+    MAP_SPIIntEnable(GSPI_BASE,SPI_INT_RX_FULL|SPI_INT_TX_EMPTY);
+
+    //
+    // Enable SPI for communication
+    //
+    MAP_SPIEnable(GSPI_BASE);
+
+    //
+    // Print mode on uart
+    //
+    Message("Enabled SPI Interface in Slave Mode\n\rReceived : ");
+}
 
 //*****************************************************************************
 //
@@ -202,9 +363,9 @@ BoardInit(void)
 {
 /* In case of TI-RTOS vector table is initialize by OS itself */
 #ifndef USE_TIRTOS
-    //
-    // Set vector table base
-    //
+  //
+  // Set vector table base
+  //
 #if defined(ccs)
     MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
 #endif
@@ -212,7 +373,6 @@ BoardInit(void)
     MAP_IntVTableBaseSet((unsigned long)&__vector_table);
 #endif
 #endif
-    
     //
     // Enable Processor
     //
@@ -221,88 +381,174 @@ BoardInit(void)
 
     PRCMCC3200MCUInit();
 }
-//****************************************************************************
+
+void test(){
+    fillScreen(WHITE);
+    delay(10);
+    //
+    //character set
+    int x=0;
+    int y=0;
+    int a=0;
+    for (a=0;a< 255; a++){
+            drawChar(x, y, a, BLACK, WHITE, 1);
+            x = x+8;
+            if (x>=128){
+                y= y + 8;
+                x= 0;
+                if (y>=128){
+                    y = 0;
+                }
+
+            }
+
+    }
+    //
+    //Hello World!
+    delay(10);
+    fillScreen(WHITE);
+    delay(10);
+    x = 0;
+    y = 0;
+    drawChar(x, y, 72, BLACK, WHITE, 1);    //H
+    x = x+8;
+    drawChar(x, y, 101, BLACK, WHITE, 1);   //e
+    x = x+8;
+    drawChar(x, y, 108, BLACK, WHITE, 1);   //l
+    x = x+8;
+    drawChar(x, y, 108, BLACK, WHITE, 1);   //l
+    x = x+8;
+    drawChar(x, y, 111, BLACK, WHITE, 1);   //o
+    x = x+8;
+    drawChar(x, y, 32, BLACK, WHITE, 1);    //space
+    x = x+8;
+    drawChar(x, y, 87, BLACK, WHITE, 1);    //W
+    x = x+8;
+    drawChar(x, y, 111, BLACK, WHITE, 1);   //o
+    x = x+8;
+    drawChar(x, y, 114, BLACK, WHITE, 1);   //r
+    x = x+8;
+    drawChar(x, y, 108, BLACK, WHITE, 1);   //l
+    x = x+8;
+    drawChar(x, y, 100, BLACK, WHITE, 1);   //d
+    x = x+8;
+    drawChar(x, y, 33, BLACK, WHITE, 1);    //!
+    x = x+8;
+    //
+    //Horizontal
+    lcdTestPattern();
+    delay(10);
+    //Vertical
+    lcdTestPattern2();
+    delay(10);
+    //lines
+    testlines(RED);
+    delay(10);
+    //fastlines
+    testfastlines(BLUE,GREEN);
+    delay(10);
+    //drawrects
+    testdrawrects(YELLOW);
+    delay(10);
+    //fillrects
+    testfillrects(MAGENTA,CYAN);
+    delay(10);
+    //fillcircles
+    testfillcircles(50, BLACK);
+    delay(10);
+    //roundrects
+    testroundrects();
+    delay(10);
+    //triangles
+    testtriangles();
+    delay(10);
+    //
+    //
+}
+//*****************************************************************************
 //
-//! Main function
+//! Main function for spi demo application
 //!
 //! \param none
-//! 
-//! This function  
-//!    1. Invokes the LEDBlinkyTask
 //!
 //! \return None.
 //
-//****************************************************************************
-int
-main()
+//*****************************************************************************
+void main()
 {
     //
     // Initialize Board configurations
     //
     BoardInit();
-    
+
     //
-    // Power on the corresponding GPIO port B for 9,10,11.
-    // Set up the GPIO lines to mode 0 (GPIO)
+    // Muxing UART and SPI lines.
     //
     PinMuxConfig();
 
+    //
+    // Enable the SPI module clock
+    //
+    MAP_PRCMPeripheralClkEnable(PRCM_GSPI,PRCM_RUN_MODE_CLK);
+
+    //
+    // Initialising the Terminal.
+    //
     InitTerm();
+
+    //
+    // Clearing the Terminal.
+    //
     ClearTerm();
 
-    Message("\t\t*************************************************\n\r");
-    Message("\t\t\t         CC3200 GPIO Application         \n\r");
-    Message("\t\t*************************************************\n\r");
-    Message("\t\t****************************************************\n\r");
-    Message("\t\t Push SW3 to start LED binary counting \n\r");
-    Message("\t\t Push SW2 to blink LEDs on and off \n\r");
-    Message("\t\t****************************************************\n\r");
-
-    GPIO_IF_LedConfigure(LED1|LED2|LED3);
-
-    GPIO_IF_LedOff(MCU_ALL_LED_IND);
-    
     //
-    // Start the LEDBlinkyRoutine
+    // Display the Banner
     //
-    int sw3 = 0;
-    int sw2 = 0;
-    int stat = 0;
+    Message("\n\n\n\r");
+    Message("\t\t   ********************************************\n\r");
+    Message("\t\t        CC3200 SPI Demo Application  \n\r");
+    Message("\t\t   ********************************************\n\r");
+    Message("\n\n\n\r");
+
+    //
+    // Reset the peripheral
+    //
+    MAP_PRCMPeripheralReset(PRCM_GSPI);
+
+    MAP_SPIReset(GSPI_BASE);
+
+        //
+        // Configure SPI interface
+        //
+    MAP_SPIConfigSetExpClk(GSPI_BASE,MAP_PRCMPeripheralClockGet(PRCM_GSPI),
+                         SPI_IF_BIT_RATE,SPI_MODE_MASTER,SPI_SUB_MODE_0,
+                         (SPI_HW_CTRL_CS |
+                         SPI_4PIN_MODE |
+                         SPI_TURBO_OFF |
+                         SPI_CS_ACTIVELOW |
+                         SPI_WL_8));
+
+    MAP_SPIEnable(GSPI_BASE);
+
+    Adafruit_Init();
+    test();
+
+
+
+#if MASTER_MODE
+
+    //MasterMain();
+
+#else
+
+    //SlaveMain();
+
+#endif
+
     while(1)
-     {    sw3 =  MAP_GPIOPinRead(GPIOA1_BASE, 0x20);
-          sw2 =  MAP_GPIOPinRead(GPIOA2_BASE, 0x40);
+    {
 
+    }
 
-        if (sw3 != 0) {
-            stat = 1;
-            Message("SW3 pressed\n\r");
-
-
-        }
-        if (sw2 != 0){
-            stat = 2;
-            Message("SW2 pressed\n\r");
-
-        }
-
-        if (stat == 1 )
-        {
-            LEDBlinkyRoutine();
-            GPIOPinWrite(GPIOA3_BASE, 0x10, 0);
-        }
-        else if (stat ==2)
-        {
-            LEDFlashRoutine();
-            GPIOPinWrite(GPIOA3_BASE, 0x10, 0x10);
-        }
-
-     }
-    return 0;
 }
 
-//*****************************************************************************
-//
-// Close the Doxygen group.
-//! @}
-//
-//*****************************************************************************
